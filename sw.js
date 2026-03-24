de6685577b04d32d9d5219cf14fa5c030a16892c
arnaudchapelle <studio@arnaudchapelle.com>
2026-03-24 17:26:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
-const CACHE_NAME = "maivi-v2";
+const CACHE_NAME = "maivi-v3";
 
+// Installation
 self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
+// Activation
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
+// Fetch (réseau d'abord, fallback cache)
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
